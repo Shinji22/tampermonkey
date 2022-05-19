41697cd342b380a16ca5d758a8b4b40fe5391db0
Patrice TANAKA <patrice.tanaka@eco-counter.com>
2022-05-19 11:57:07 +0200

    feat: disable security check

--- eco-visio.js
+++ eco-visio.js
@@ -1,5 +1,16 @@
 (function() {
     'use strict';
 
-	alert('Yeah')
+	const DISABLE_PASSWORD_SECURITY_CHECK = true; // Désactive la modale forçant à modifier son mot de passe
+
+    const disablePasswordSecurity = () => {
+        const passwordSecurityURLPattern = /.*\/confirm\/#strengthenPassword.*/;
+        window.addEventListener('popstate', function (event) {
+            if (window.location.href.match(passwordSecurityURLPattern)) {
+             document.location.replace(window.location.href.replace(/confirm\/#strengthenPassword.*/, ''));
+            }
+        });
+    };
+
+    if (DISABLE_PASSWORD_SECURITY_CHECK) disablePasswordSecurity();
 })();
